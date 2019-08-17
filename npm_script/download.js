64485bbdc2790d854debedb1d87c579de00373b5
sasaplus1 <sasaplus1@gmail.com>
2019-08-17 14:45:56 +0900

    fix download.js

--- npm_script/download.js
+++ npm_script/download.js
@@ -1,19 +1,19 @@
-#!/usr/bin/env node
-
 const querystring = require('querystring');
 
 const iconvLite = require('iconv-lite');
 const request = require('request');
 
-const parameter = querystring.stringify({ id: 'd9ad35a5-6c9c-4127-bdbe-aa138fdffe42' });
-const URL = `https://www.data.go.jp/data/api/action/resource_show?${parameter}`
+const parameter = querystring.stringify({
+  id: 'd9ad35a5-6c9c-4127-bdbe-aa138fdffe42'
+});
+const URL = `https://www.data.go.jp/data/api/action/resource_show?${parameter}`;
 
 request(URL, function(error, response, body) {
   if (error) {
     throw error;
   }
 
-  const { result, success } = JSON.parse(body);
+  const { result } = JSON.parse(body);
   const { url } = result;
 
   request
