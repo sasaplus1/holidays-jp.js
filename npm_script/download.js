a1c952da8fda6371bf73f13b1f1e7bb623e46b19
sasaplus1 <sasaplus1@gmail.com>
2019-08-17 14:12:30 +0900

    add download script

---
+++ npm_script/download.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const querystring = require('querystring');
+
+const iconvLite = require('iconv-lite');
+const request = require('request');
+
+const parameter = querystring.stringify({ id: 'd9ad35a5-6c9c-4127-bdbe-aa138fdffe42' });
+const URL = `https://www.data.go.jp/data/api/action/resource_show?${parameter}`
+
+request(URL, function(error, response, body) {
+  if (error) {
+    throw error;
+  }
+
+  const { result, success } = JSON.parse(body);
+  const { url } = result;
+
+  request
+    .get(url)
+    .pipe(iconvLite.decodeStream('cp932'))
+    .pipe(iconvLite.encodeStream('utf8'))
+    .pipe(process.stdout);
+});
