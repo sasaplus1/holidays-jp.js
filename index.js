6ed49a7a029def365ecf68cffea73d7ce56fc177
sasaplus1 <sasaplus1@gmail.com>
2019-08-18 16:47:26 +0900

    add some scripts

---
+++ index.js
@@ -0,0 +1,81 @@
+const data = require('./data');
+
+const toString = Object.prototype.toString;
+
+// NOTE: JST is UTC+9:00
+const jstOffset = 1000 * 60 * 60 * 9;
+
+// NOTE: maybe faster than before reverse array when if use Array#filter, Array#find and etc.
+const holidays = data.holidays.reverse().map(function(holiday) {
+  const { date, name } = holiday;
+
+  const [year, month, day] = date.split('-').map(function(number) {
+    return parseInt(number, 10);
+  });
+
+  const startDate = new Date(
+    Date.UTC(year, month - 1, day, 0, 0, 0, 0) - jstOffset
+  );
+  const endDate = new Date(
+    Date.UTC(year, month - 1, day + 1, 0, 0, 0, 0) - jstOffset - 1
+  );
+
+  return {
+    date,
+    endDate,
+    name,
+    startDate
+  };
+});
+
+const holidayMap = holidays.reduce(function(result, holiday) {
+  const { date } = holiday;
+  const [year, month, day] = date.split('-');
+
+  if (!result[year]) {
+    result[year] = {};
+  }
+
+  if (!result[year][month]) {
+    result[year][month] = {};
+  }
+
+  result[year][month][day] = holiday;
+
+  return result;
+}, {});
+
+/**
+ * return true if date is holiday
+ *
+ * @param {Date} date
+ * @return {boolean}
+ */
+function isHoliday(date) {
+  if (toString.call(date) !== '[object Date]') {
+    throw new TypeError('date must be a Date: ' + date);
+  }
+
+  const time = date.getTime();
+
+  for (let holiday of holidays) {
+    const { startDate, endDate } = holiday;
+    const endTime = endDate.getTime();
+
+    if (startDate.getTime() <= time && endTime >= time) {
+      return true;
+    }
+
+    if (time > endTime) {
+      break;
+    }
+  }
+
+  return false;
+}
+
+module.exports = {
+  holidayMap,
+  holidays,
+  isHoliday
+};
