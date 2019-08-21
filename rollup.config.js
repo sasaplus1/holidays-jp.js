5e390a87317a246f856fc26d15d0c09b339a6a2d
sasaplus1 <sasaplus1@gmail.com>
2019-08-21 21:38:38 +0900

    update tsconfig.json and rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,40 @@
 import typescript from 'rollup-plugin-typescript';
 
+import { compilerOptions } from './tsconfig';
+
 const config = [];
 
 if (process.env.data) {
-  config.push({
-    input: './data.ts',
-    output: {
-      file: './data.mjs',
-      format: 'esm'
+  config.push(
+    {
+      input: './data.ts',
+      output: {
+        file: './data.mjs',
+        format: 'esm'
+      },
+      plugins: [
+        typescript({
+          ...compilerOptions,
+          module: 'ESNext',
+          target: 'ESNext'
+        })
+      ]
     },
-    plugins: [
-      typescript({
-        module: 'ESNext',
-        target: 'ESNext'
-      })
-    ]
-  });
+    {
+      input: './data.ts',
+      output: {
+        file: './data.js',
+        format: 'cjs'
+      },
+      plugins: [
+        typescript({
+          ...compilerOptions,
+          module: 'ESNext',
+          target: 'ESNext'
+        })
+      ]
+    }
+  );
 }
 
 export default config;
