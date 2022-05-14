a0f573e0cb991793a16091a6edfabf9d417af789
sasaplus1 <sasaplus1@gmail.com>
2022-05-14 23:15:55 +0900

    fix some fixers

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,7 @@
 module.exports = {
   '*.+(js|mjs|ts)': ['eslint', 'prettier --check'],
   '*.+(md|yml)': 'prettier --check',
-  '!(package|package-lock).json': 'prettier --check --parser json-stringify',
-  'package.json': [
-    'npx fixpack --dryRun',
-    'prettier --check --parser json-stringify'
-  ],
+  '!(package|package-lock).json': 'prettier --check',
+  'package.json': ['npx fixpack --dryRun', 'prettier --check'],
   'package-lock.json': 'node -e "process.exitCode = 1"'
 };
